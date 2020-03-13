4b9549564cee75ab0f8d72fb851c6a72cd77342d
Bird <29718216+xXBuildersXx@users.noreply.github.com>
2020-03-13 14:38:36 -0600

    Revert "Update threed.js"
    This reverts commit 8bf9ce6f82938b0899c08c6242f35694270fe651.

--- Starlink_files/threed.js
+++ Starlink_files/threed.js
@@ -217,7 +217,22 @@
     var arrayExecuter = new ArrayExecuter();
 	var stepComplete = arrayExecuter.stepComplete_instant.bind(arrayExecuter);
 
+	function initExperience() {
+		document.getElementById("interactive").addEventListener( 'mousedown', onDocumentMouseDown, false );
+		document.getElementById("interactive").addEventListener( 'mousemove', onDocumentMouseMove, false );
+		document.getElementById("interactive").addEventListener( 'mouseup', onDocumentMouseUp, false );
+		document.getElementById("interactive").addEventListener( 'mouseleave', onDocumentMouseLeave, false );
+
+		document.getElementById("interactive").addEventListener( 'touchstart', onDocumentTouchStart, false );
+		document.getElementById("interactive").addEventListener( 'touchmove', onDocumentTouchMove, false );
+		document.getElementById("interactive").addEventListener( 'touchend', onDocumentTouchEnd, false );
+		
 		// NO MOUSE SCROLLING THIS TIME
+    	document.getElementById("interactive").addEventListener('mousewheel', onMouseWheel, false);
+
+		document.addEventListener('gesturestart', function (e) {
+			e.preventDefault();
+		});
 
 		window.addEventListener( 'resize', onWindowResize, false );
 		onWindowResize();
